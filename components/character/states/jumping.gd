47ff6637e216ff6d4b486c2558ee4799d4f47832
Neeraga Nagaraj <neerava.nraj@gmail.com>
2026-02-28 14:44:34 +0530

    Jumping timer

---
+++ components/character/states/jumping.gd
@@ -0,0 +1,16 @@
+extends PlayerState
+
+@onready var jump_timer: Timer = $JumpTimer 
+
+
+
+func _on_jumping_state_entered() -> void:
+	if jump_timer.is_stopped(): jump_timer.stop()
+	jump_timer.start()
+
+func _on_jump_timer_timeout() -> void:
+	if not player: return
+	player._state_chart.send_event(&"onFalling")
+
+func _on_jumping_state_exited() -> void:
+	jump_timer.stop()
