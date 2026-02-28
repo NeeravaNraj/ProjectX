332f78bcbeb315afbe4b06ab980f966048ff8707
Neeraga Nagaraj <neerava.nraj@gmail.com>
2026-02-28 11:58:19 +0530

    Segregate jump and falling states

--- components/character/states/grounded_state.gd
+++ components/character/states/grounded_state.gd
@@ -5,7 +5,8 @@ func _on_grounded_state_physics_processing(delta: float) -> void:
 	if not player: return
 	
 	if not player.is_on_floor():
-		player._state_chart.send_event("onAirborne")
+		player._state_chart.send_event(&"onAirborne")
 	
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed(&"jump"):
 		player.jump()
+		player._state_chart.send_event(&"onJumping")
