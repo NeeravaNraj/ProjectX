d5238422c5554176246a6da8d570d12343754771
Neeraga Nagaraj <neerava.nraj@gmail.com>
2026-02-28 21:18:12 +0530

    Start using resources to model data

---
+++ resources/character/player_stats.gd
@@ -0,0 +1,18 @@
+class_name PlayerStats extends Resource
+
+
+
+@export var move_speed_modifier: float = 0.0
+@export var move_speed_sprint: float = 4.0:
+	get:
+		return move_speed_sprint + move_speed_modifier
+@export var move_speed: float = 8.0:
+	get:
+		return move_speed + move_speed_modifier
+
+@export var jump_velocity_modifier: float = 0.0
+@export var jump_velocity: float = 10.0:
+	get:
+		return jump_velocity + jump_velocity_modifier
+
+@export var grapple_velocity: float = 35.0
