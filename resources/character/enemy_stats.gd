67e99023bfd473603b312d71821d092de08aa098
Neeraga Nagaraj <neerava.nraj@gmail.com>
2026-03-02 23:43:10 +0530

    Adding enemy stats

---
+++ resources/character/enemy_stats.gd
@@ -0,0 +1,8 @@
+class_name EnemyStats extends Resource
+
+@export var attack_damage: float = 10
+@export var attack_range: float = 1.5
+@export var attack_speed: float = 1.0
+
+@export var move_speed: float = 8.0
+@export var search_speed: float = 6.0
