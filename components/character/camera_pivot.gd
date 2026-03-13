012e0c6b41f95bfcb3c4385d5bfc4969f88cd417
Neeraga Nagaraj <neerava.nraj@gmail.com>
2026-03-14 00:00:52 +0530

    Use lerp for camera tilt

--- components/character/camera_pivot.gd
+++ components/character/camera_pivot.gd
@@ -20,7 +20,7 @@ func _unhandled_input(event: InputEvent) -> void:
 		mouse_input += event.screen_relative * player.mouse_sens
 
 	
-func  _physics_process(_delta: float):
+func  _physics_process(delta: float):
 	current_rotation.x = clamp(current_rotation.x - mouse_input.y, deg_to_rad(-80), deg_to_rad(90))
 	current_rotation.y -= mouse_input.x
 	
@@ -32,13 +32,12 @@ func  _physics_process(_delta: float):
 	
 	mouse_input = Vector2.ZERO
 	
-	_tilt_camera()
+	_tilt_camera(delta)
 	
-func _tilt_camera():
+func _tilt_camera(delta: float):
 	var move_direction = player._velocity.raw_direction
 	var side = Vector3.FORWARD * (MAX_TILT_ANGLE * move_direction.x)
 	var forward = Vector3.RIGHT * ((MAX_TILT_ANGLE / 2.0) * move_direction.y)
 	var rot = forward + side
 	
-	var tween = create_tween()
-	tween.tween_property(player.camera, "rotation", rot, 0.3)
+	player.camera.rotation = player.camera.rotation.lerp(rot, delta * 6.0)
