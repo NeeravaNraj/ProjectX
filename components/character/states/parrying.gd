4524368e4b92e4f79b1ebe7e23ad3ace249e18e3
Neeraga Nagaraj <neerava.nraj@gmail.com>
2026-03-14 18:07:10 +0530

    Slow down player movement when parrying

--- components/character/states/parrying.gd
+++ components/character/states/parrying.gd
@@ -1,9 +1,12 @@
 extends PlayerState
 
+const PARRY_SPEED := -2.0
 
 func _on_parrying_state_entered() -> void:
+	player.player_stats.move_speed_modifier += PARRY_SPEED
 	player.fp_rig.parry(true)
 
 
 func _on_parrying_state_exited() -> void:
+	player.player_stats.move_speed_modifier -= PARRY_SPEED
 	player.fp_rig.parry(false)
