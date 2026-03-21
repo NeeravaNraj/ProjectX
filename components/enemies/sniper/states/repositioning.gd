fc3b54b0729a1b5e276e2159ff7db59b660d797c
Stratip <85007222+Stratip@users.noreply.github.com>
2026-03-21 20:56:35 +0530

    Update sniper enemy.

--- components/enemies/sniper/states/repositioning.gd
+++ components/enemies/sniper/states/repositioning.gd
@@ -1,13 +1,19 @@
 extends SniperEnemyState
 
 
+@onready var dummy_timer: Timer = $DummyTimer
+
+
 func _on_repositioning_state_entered() -> void:
 	print("Begin repositioning")
+	dummy_timer.start()
 
 
 func _on_repositioning_state_physics_processing(delta: float) -> void:
 	pass
 
+func _on_dummy_timer_timeout() -> void:
+	sniper_enemy.state_chart.send_event("onAiming")
 
 func _on_repositioning_state_exited() -> void:
 	print("Done repositioning")
--- components/enemies/sniper/states/shooting.gd
+++ components/enemies/sniper/states/shooting.gd
@@ -1,1 +1,6 @@
 extends SniperEnemyState
+
+
+func _on_shooting_state_entered() -> void:
+	print("Shooting...")
+	sniper_enemy.state_chart.send_event("onRepositioning")
