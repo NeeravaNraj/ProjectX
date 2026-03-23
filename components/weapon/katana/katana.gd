f33c6e0c403656f0d35f5fdfbc07d8eefbe09493
Neeraga Nagaraj <neerava.nraj@gmail.com>
2026-03-23 22:31:14 +0530

    Tweak hitbox

--- components/weapon/katana/katana.gd
+++ components/weapon/katana/katana.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 	box = attack_area_shape.shape as BoxShape3D
 	assert(box, "Expected BoxShape3D for attack area in Katana")
 	
-	box.size = Vector3(2.5, 1, 2)
+	box.size = Vector3(4, 1, 3)
 
 func animate_attack():
 	pass
