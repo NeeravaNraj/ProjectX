639f4964ffe8b0dfb1bca9d3ff83af1534013fb4
Neeraga Nagaraj <neerava.nraj@gmail.com>
2026-04-10 22:16:12 +0530

    Melee enemy update

--- components/enemies/melee/melee_enemy.gd
+++ components/enemies/melee/melee_enemy.gd
@@ -3,26 +3,23 @@ class_name MeleeEnemy extends CharacterBody3D
 @export var stats: EnemyStats
 @export var enabled: bool = true
 
-@onready var hurtbox: HurtBox = $HurtBox
-@onready var hitbox: HitBox = $Areas/HitBox
+@onready var enemy_skin = $MeleeEnemySkin
+@onready var hurtbox: HurtBox = $Areas/HurtBox
 @onready var state_chart: StateChart = %StateChart
 @onready var _velocity: VelocityComponent = $VelocityComponent
 @onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
-@onready var player_detection_area: Area3D = $Areas/PlayerDetectionArea
-@onready var detection_shape: CollisionShape3D = $Areas/PlayerDetectionArea/PlayerDetectionShape
 
 var starting_position: Vector3
 var speed: float
 
-var player: Variant = null
+var player: Player
 var last_player_position = null
 var hit_direction = Vector3.ZERO
 
 signal alert(current_position: Vector3, player_position: Vector3)
 
 func get_vistion_radius():
-	var shape = detection_shape.shape as SphereShape3D
-	return shape.radius
+	return stats.vision_distance
 
 func get_player_distance():
 	if not player: return INF
@@ -32,30 +29,33 @@ func get_player_distance():
 func _ready() -> void:
 	starting_position = global_position
 	state_chart.send_event(&"onIdle")
+	player = get_tree().get_first_node_in_group(&"player")
 
 func move_towards_target():
 	if navigation_agent.is_navigation_finished(): return
 	var current_agent_position: Vector3 = global_position
 	var next_path_position: Vector3 = navigation_agent.get_next_path_position()
 	var direction = current_agent_position.direction_to(next_path_position)
-
+	enemy_skin.locomotion(MeleeEnemySkin.MovementStates.Running)
+	lookat(direction)
 	_velocity.set_velocity(direction * speed)
+
+func lookat(dir: Vector3):
+	dir.y = 0
+	look_at(global_position + dir, Vector3.UP)
 
 func lookat_player():
 	if not player: return
-	var dir = player.global_position - global_position
-	dir.y = 0
-	look_at(global_position + dir, Vector3.UP)
+	lookat(player.global_position - global_position)
 
 func _on_health_damaged(amount: int) -> void:
 	print("Enemy DAMAGED ", amount)
 
-
 func _on_health_death() -> void:
 	queue_free()
-
 
 func _on_hurt_box_hit(packet: DamagePacket) -> void:
 	hit_direction = packet.attack_direction * packet.knockback_modifier
 	hit_direction.y += 0.5 * packet.knockback_modifier
-	state_chart.send_event(&"onDamaged")
+	enemy_skin.stagger()
+	
