931943a6a0a50e44fefee57567c4dc6cd06c072f
Neeraga Nagaraj <neerava.nraj@gmail.com>
2026-04-10 22:18:25 +0530

    Simplify searching state

--- components/enemies/melee/states/searching.gd
+++ components/enemies/melee/states/searching.gd
@@ -1,27 +1,30 @@
 extends MeleeEnemyState
 
 @onready var search_timer = $SearchTimer
-
+@onready var anim_timer = $SearchAnimTimer
 
 func _on_searching_state_entered() -> void:
 	if not search_timer.is_stopped(): search_timer.stop()
+	anim_timer.stop()
 	search_timer.start()
 
 func _on_search_timer_timeout() -> void:
-	melee_enemy.player = null
-	melee_enemy.last_player_position = null
-	melee_enemy.navigation_agent.set_target_position(melee_enemy.starting_position)
-	melee_enemy.state_chart.send_event(&"onIdle")
+	await melee_enemy.enemy_skin.anim_tree.animation_finished
+	melee_enemy.state_chart.send_event(&"onReturning")
 
 func _on_searching_state_exited() -> void:
 	search_timer.stop()
-
 
 func _on_searching_state_physics_processing(_delta: float) -> void:
 	if not melee_enemy: return
 
 	if melee_enemy.get_player_distance() < melee_enemy.get_vistion_radius():
 		melee_enemy.state_chart.send_event(&"onAggravated")
-
+	
 	melee_enemy.speed = melee_enemy.stats.search_speed
-	melee_enemy.move_towards_target()
+	if melee_enemy.navigation_agent.is_navigation_finished() and melee_enemy.velocity.length() == 0:
+		if anim_timer.is_stopped():
+			melee_enemy.enemy_skin.searching()
+			anim_timer.start(randf_range(4, 7))
+	else:
+		melee_enemy.move_towards_target()
