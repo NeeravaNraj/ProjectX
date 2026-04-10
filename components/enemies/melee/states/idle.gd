19f17f5de52aa2c87b226e22e6fa49fc4cde495f
Neeraga Nagaraj <neerava.nraj@gmail.com>
2026-04-10 22:18:12 +0530

    Simplify player detection

--- components/enemies/melee/states/idle.gd
+++ components/enemies/melee/states/idle.gd
@@ -4,10 +4,9 @@ extends MeleeEnemyState
 func _on_idle_state_physics_processing(delta: float) -> void:
 	if not (melee_enemy or melee_enemy.enabled): return
 
-	for body in melee_enemy.player_detection_area.get_overlapping_bodies():
-		if body is Player:
-			melee_enemy.player = body
-			melee_enemy.state_chart.send_event(&"onAggravated")
-
+	if melee_enemy.get_player_distance() <= melee_enemy.get_vistion_radius():
+		melee_enemy.state_chart.send_event(&"onAggravated")
+	
+	melee_enemy.enemy_skin.locomotion(MeleeEnemySkin.MovementStates.Idle)
 	melee_enemy.speed = melee_enemy.stats.move_speed
 	melee_enemy.move_towards_target()
