a1a4dc5e2a5b1b11553fdbed2a2a0e42ac3f71ee
Neeraga Nagaraj <neerava.nraj@gmail.com>
2026-04-10 22:17:55 +0530

    Make attacking cooldown be dependent on animation time

--- components/enemies/melee/states/attacking.gd
+++ components/enemies/melee/states/attacking.gd
@@ -1,14 +1,22 @@
 extends MeleeEnemyState
 
 @onready var attack_timer: Timer = $AttackTimer
+var can_attack: bool = true
 
 func _on_attacking_state_physics_processing(delta: float) -> void:
 	if not melee_enemy: return
 
-	if melee_enemy.get_player_distance() > melee_enemy.stats.attack_range:
+	if can_attack && melee_enemy.get_player_distance() > melee_enemy.stats.attack_range:
 		melee_enemy.state_chart.send_event(&"onAggravated")
-	
-	if attack_timer.is_stopped():
+	elif can_attack:
 		var packet = DamagePacket.new(melee_enemy.stats.attack_damage)
-		melee_enemy.hitbox.attack(packet)
-		attack_timer.start()
+		melee_enemy.enemy_skin.attack(packet)
+		can_attack = false
+	
+	melee_enemy.enemy_skin.locomotion(MeleeEnemySkin.MovementStates.Idle)
+
+func _on_melee_enemy_skin_attack_finished() -> void:
+	can_attack = true
+
+func _on_attacking_state_entered() -> void:
+	can_attack = true
