9ce3ebbca73724434db271edd0628f1c4299a6ef
Neeraga Nagaraj <neerava.nraj@gmail.com>
2026-04-11 14:38:46 +0530

    Fix stupid ahh cylinder laser clip mabob (#1)

--- components/enemies/sniper/laser_beam.gd
+++ components/enemies/sniper/laser_beam.gd
@@ -3,6 +3,6 @@ class_name LaserBeam extends Node3D
 @onready var beam_mesh = $BeamMesh
 var laser_length: float = 0.0
 
-func _process(_delta: float) -> void:
-	self.beam_mesh.position.z = -self.laser_length / 2.0
-	self.beam_mesh.mesh.height = self.laser_length
+func _physics_process(delta: float) -> void:
+	beam_mesh.position.z = -laser_length / 2.0
+	beam_mesh.scale.y = laser_length
--- components/enemies/sniper/states/aiming.gd
+++ components/enemies/sniper/states/aiming.gd
@@ -2,7 +2,8 @@ extends SniperEnemyState
 
 
 @onready var aim_timer: Timer = $AimTimer
-
+const LEN_CHANGE_FACTOR = 10.0
+var prev_length = 0.0
 
 func _on_aiming_state_entered() -> void:
 	sniper_enemy.laser_beam.visible = true
@@ -11,15 +12,12 @@ func _on_aiming_state_entered() -> void:
 func _on_aiming_state_physics_processing(delta: float) -> void:
 	if not sniper_enemy or not sniper_enemy.player: return
 
-	var current_aim = sniper_enemy.laser_beam.global_basis.get_rotation_quaternion()
-	var ideal_aim = sniper_enemy.laser_beam.global_transform.looking_at(
+	var current_aim = sniper_enemy.aim_pointer.global_basis.get_rotation_quaternion()
+	var ideal_aim = sniper_enemy.aim_pointer.global_transform.looking_at(
 		sniper_enemy.player.global_position, Vector3.UP
 	).basis.get_rotation_quaternion()
 
-	var new_aim = current_aim.slerp(
-		ideal_aim, delta * 15.0
-	)
-
+	var new_aim = current_aim.slerp(ideal_aim, delta * LEN_CHANGE_FACTOR)
 	sniper_enemy.aim_pointer.global_basis = new_aim
 
 	var space_state = sniper_enemy.get_world_3d().direct_space_state
@@ -28,13 +26,15 @@ func _on_aiming_state_physics_processing(delta: float) -> void:
 		sniper_enemy.global_position + sniper_enemy.aim_pointer.global_basis * Vector3(0.0, 0.0, -sniper_enemy.stats.aim_range)
 	)
 	var result = space_state.intersect_ray(query)
-
+	
 	if result:
-		sniper_enemy.laser_beam.laser_length = (result.position - sniper_enemy.global_position).length()
+		var laser_length: float = (result.position - sniper_enemy.global_position).length()
+		sniper_enemy.laser_beam.laser_length = laser_length
 	else:
 		sniper_enemy.laser_beam.laser_length = sniper_enemy.stats.aim_range
 
 func _on_aim_timer_timeout() -> void:
+	return
 	sniper_enemy.state_chart.send_event(&"onShooting")
 
 func _on_aiming_state_exited() -> void:
