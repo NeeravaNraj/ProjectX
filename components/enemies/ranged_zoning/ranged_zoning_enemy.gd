b5631102647f5fb76aeb947cff35eaff1185c63a
Neeraga Nagaraj <neerava.nraj@gmail.com>
2026-04-16 20:25:29 +0530

    Ranged zoning enemy implementation

--- components/enemies/ranged_zoning/ranged_zoning_enemy.gd
+++ components/enemies/ranged_zoning/ranged_zoning_enemy.gd
@@ -1,1 +1,20 @@
 class_name RangedZoningEnemy extends BaseEnemy
+
+@onready var gun: MeshInstance3D = $ViewModel/Gun
+@onready var grenade_launcher: GrenadeLauncher = $GrenadeLaunchCtrl
+
+func _ready() -> void:
+	init()
+
+func aim():
+	var target = predict_next_player_position(stats.aim_prediction_time)
+	var pitch = grenade_launcher.get_pitch(target)
+	gun.rotation.x = -pitch
+
+func attack():
+	if not enabled: return
+	var target = predict_next_player_position(stats.aim_prediction_time)
+	grenade_launcher.launch(target)
+
+func _on_health_death() -> void:
+	queue_free()
