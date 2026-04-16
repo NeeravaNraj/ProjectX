a67474d3a798a31357bc0bbc250cb3074493da39
Neeraga Nagaraj <neerava.nraj@gmail.com>
2026-04-16 20:25:20 +0530

    Zoning grenade implementation

--- components/enemies/ranged_zoning/zoning_grenade.gd
+++ components/enemies/ranged_zoning/zoning_grenade.gd
@@ -1,28 +1,29 @@
-class_name ZoningGrenade extends Node3D
+class_name ZoningGrenade extends RigidBody3D
 
 @onready var hit_timer: Timer = $HitTimer
 @onready var despawn_timer: Timer = $DespawnTimer
-@onready var rigid_body: RigidBody3D = $RigidBody3D
-@onready var visual_body: MeshInstance3D = $RigidBody3D/VisualBody
-@onready var explosion_mesh: MeshInstance3D = $RigidBody3D/Explosion
-@onready var explosion_area: HitBox = $RigidBody3D/ExplosionArea
-@onready var explosion_collision_shape: CollisionShape3D = $RigidBody3D/ExplosionArea/CollisionShape3D
+@onready var bullet_hitbox: HitBox = $BulletHitbox
+@onready var explosion_area: HitBox = $ExplosionArea
+@onready var visual_body: MeshInstance3D = $VisualBody
+@onready var explosion_fog: FogVolume = $ExplosionFog
+@onready var explosion_mesh: MeshInstance3D = $Explosion
+@onready var explosion_collision_shape: CollisionShape3D = $ExplosionArea/CollisionShape3D
 
-var packet: DamagePacket = DamagePacket.new(25.0)
-
-#func _init(p_packet: DamagePacket) -> void:
-	#packet = p_packet
+var packet: DamagePacket:
+	set(p):
+		packet = p
+		bullet_hitbox.default_packet = p
 
 func _ready() -> void:
-	rigid_body.get_colliding_bodies()
 	set_physics_process(false)
 
-func _on_rigid_body_3d_body_entered(_body: Node) -> void:
-	rigid_body.freeze = true
+func _on_body_entered(_body: Node) -> void:
+	freeze = true
+	collision_mask = 0
 	visual_body.visible = false
+	explosion_fog.visible = true
 	explosion_mesh.visible = true
 	explosion_area.monitoring = true
-	rigid_body.collision_layer = 0
 	
 	despawn_timer.start()
 	set_physics_process(true)
@@ -34,14 +35,16 @@ func _grow_blast_area(radius: float):
 	var visual_shape = explosion_mesh.mesh as SphereMesh
 	var collision_shape = explosion_collision_shape.shape as SphereShape3D
 	
-	visual_shape.radius = radius
+	var increased_radius = radius + 2.5
+	var reduced_radius = radius - 3.5
 	collision_shape.radius = radius
-	visual_shape.height = radius * 2
-
+	visual_shape.radius = reduced_radius
+	visual_shape.height = reduced_radius * 2
+	explosion_fog.size = Vector3.ONE * increased_radius
 
 func _on_despawn_timer_timeout() -> void:
 	var tween = create_tween()
-	tween.tween_method(_grow_blast_area, 5.0, 0.0, 0.5)
+	tween.tween_method(_grow_blast_area, 5.0, 0.01, 0.5)
 	await tween.finished
 	queue_free()
 
@@ -49,6 +52,3 @@ func _physics_process(delta: float) -> void:
 	if hit_timer.is_stopped():
 		hit_timer.start()
 		explosion_area.attack(packet)
-
-func _on_explosion_area_area_entered(area: Area3D) -> void:
-	print(area)
