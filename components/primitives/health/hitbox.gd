5f3db895c3e8744d08ecef58dbe9cdd54349ed11
Neeraga Nagaraj <neerava.nraj@gmail.com>
2026-04-16 20:26:06 +0530

    Add auto hit functionality on hitbox

--- components/primitives/health/hitbox.gd
+++ components/primitives/health/hitbox.gd
@@ -1,8 +1,15 @@
 class_name HitBox extends Area3D
 
+@export var auto_hit: bool = false
 @export var exclude: Array[CollisionObject3D]
 
 signal hit(hurtbox: HurtBox, packet: DamagePacket)
+
+var default_packet: DamagePacket
+
+func _ready() -> void:
+	if auto_hit:
+		area_entered.connect(_on_area_entered)
 
 func attack(packet: DamagePacket):
 	for body in get_overlapping_areas():
@@ -12,3 +19,7 @@ func attack(packet: DamagePacket):
 		if hurtbox:
 			hurtbox.damage(packet)
 			hit.emit(hurtbox, packet)
+
+
+func _on_area_entered(area: Area3D) -> void:
+	if default_packet: attack(default_packet)
