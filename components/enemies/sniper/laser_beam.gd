681555a63ed52c80334424c19f43cf34183a4004
Stratip <85007222+Stratip@users.noreply.github.com>
2026-04-26 20:11:19 +0530

    Sniper enemy cood update**__'__**s and improvement**__'__**s.

--- components/enemies/sniper/laser_beam.gd
+++ components/enemies/sniper/laser_beam.gd
@@ -4,7 +4,7 @@ class_name LaserBeam extends Node3D
 @export var max_laser_length: float = 10000.0
 
 @onready var space_state = get_world_3d().direct_space_state
-@onready var beam_mesh = $BeamMesh
+@onready var beam_mesh: MeshInstance3D = $BeamMesh
 
 
 func _physics_process(delta: float) -> void:
@@ -12,7 +12,18 @@ func _physics_process(delta: float) -> void:
 		if self.visible: self.visible = false
 		return
 
-	if not self.visible: self.visible = true
+	if not self.visible:
+		var material = self.beam_mesh.get_active_material(0)
+		material.albedo_color.a = 0.0
+		var tween = create_tween()
+		tween.tween_property(
+			material,
+			"albedo_color:a",
+			1.0,
+			2.5
+		)
+
+		self.visible = true
 
 	var from = global_position
 	var to = global_position + (-global_transform.basis.z * max_laser_length)
--- components/enemies/sniper/sniper_enemy.gd
+++ components/enemies/sniper/sniper_enemy.gd
@@ -9,16 +9,28 @@ func _ready() -> void:
 	self.init()
 
 
+func begin_aiming():
+	if not enabled: return
+	self.sniper.begin_aiming()
+
+
+func stop_aiming():
+	if not enabled: return
+	self.sniper.stop_aiming()
+
+
 func aim():
 	if not enabled: return
 	var target = predict_next_player_position(stats.aim_prediction_time)
+	var old_rot = self.aim_pointer.rotation
 	self.aim_pointer.look_at(target)
+	var new_rot = self.aim_pointer.rotation
+	var slerped = old_rot.slerp(new_rot, 0.75)
+	self.aim_pointer.rotation = slerped
 
 
 func attack():
 	if not enabled: return
-	var target = predict_next_player_position(stats.aim_prediction_time)
-	self.aim_pointer.look_at(target)
 	var collider = self.sniper.shoot()
 	if collider:
 		collider.damage(self.packet)
